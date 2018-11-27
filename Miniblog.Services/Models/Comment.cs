4d78812fcc5f354dca3759d923c82e71cfb42c01
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2018-11-27 15:20:16 +0100

    Removed unused things from models.

--- Miniblog.Services/Models/Comment.cs
+++ Miniblog.Services/Models/Comment.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace Miniblog.Services.Models
 {
@@ -22,28 +21,5 @@ namespace Miniblog.Services.Models
         public DateTime PubDate { get; set; } = DateTime.UtcNow;
 
         public bool IsAdmin { get; set; }
-
-        public string GetGravatar()
-        {
-            using (var md5 = System.Security.Cryptography.MD5.Create())
-            {
-                byte[] inputBytes = Encoding.UTF8.GetBytes(Email.Trim().ToLowerInvariant());
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-
-                // Convert the byte array to hexadecimal string
-                var sb = new StringBuilder();
-                for (int i = 0; i < hashBytes.Length; i++)
-                {
-                    sb.Append(hashBytes[i].ToString("X2"));
-                }
-
-                return $"https://www.gravatar.com/avatar/{sb.ToString().ToLowerInvariant()}?s=60&d=blank";
-            }
-        }
-
-        public string RenderContent()
-        {
-            return Content;
-        }
     }
 }
--- Miniblog.Services/Models/Post.cs
+++ Miniblog.Services/Models/Post.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Miniblog.Services.Models
 {
@@ -43,11 +42,6 @@ namespace Miniblog.Services.Models
             return $"/blog/{System.Net.WebUtility.UrlEncode(Slug)}/";
         }
 
-        public bool AreCommentsOpen(int commentsCloseAfterDays)
-        {
-            return PubDate.AddDays(commentsCloseAfterDays) >= DateTime.UtcNow;
-        }
-
         public static string CreateSlug(string title)
         {
             title = title.ToLowerInvariant().Replace(" ", "-");
@@ -84,21 +78,6 @@ namespace Miniblog.Services.Models
             }
 
             return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
-        }
-
-        public string RenderContent()
-        {
-            var result = Content;
-            if (!string.IsNullOrEmpty(result))
-            {
-                // Set up lazy loading of images/iframes
-                result = result.Replace(" src=\"", " src=\"data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==\" data-src=\"");
-
-                // Youtube content embedded using this syntax: [youtube:xyzAbc123]
-                var video = "<div class=\"video\"><iframe width=\"560\" height=\"315\" title=\"YouTube embed\" src=\"about:blank\" data-src=\"https://www.youtube-nocookie.com/embed/{0}?modestbranding=1&amp;hd=1&amp;rel=0&amp;theme=light\" allowfullscreen></iframe></div>";
-                result = Regex.Replace(result, @"\[youtube:(.*?)\]", m => string.Format(video, m.Groups[1].Value));
-            }
-            return result;
         }
     }
 }
