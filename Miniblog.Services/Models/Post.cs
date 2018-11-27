4a127a081d60c03be9e9a0700129db4f9afa0419
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2018-11-27 15:34:33 +0100

    Attributes not needed here

--- Miniblog.Services/Models/Post.cs
+++ Miniblog.Services/Models/Post.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Text;
 
 namespace Miniblog.Services.Models
 {
     public class Post
     {
-        [Required]
         public string ID { get; set; } = DateTime.UtcNow.Ticks.ToString();
 
-        [Required]
         public string Title { get; set; }
 
         public string Slug { get; set; }
 
-        [Required]
         public string Excerpt { get; set; }
 
-        [Required]
         public string Content { get; set; }
 
         public DateTime PubDate { get; set; } = DateTime.UtcNow;
